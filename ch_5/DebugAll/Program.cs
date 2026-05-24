849f499da76fa0ad778db583aed07bc0819a71d0
Rohan Parkes <rparkes03@gmail.com>
2026-05-24 13:41:30 +1000

    Finished Ch 5.

---
+++ ch_5/DebugAll/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace DebugAll;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    var r = new Random(DateTime.Now.GetHashCode());
+
+    var stopperSequence = new Subject<bool>();
+
+    //an infinite message source of integer numbers
+    //running at 10hz
+    var source = Observable.Interval(TimeSpan.FromMilliseconds(100))
+        .Select(x => r.Next(1, 20))
+        //take only until we press RETURN
+        .TakeUntil(stopperSequence);
+
+    source.Subscribe(x => Console.WriteLine(x));
+
+    //we want message metadata
+    var all = source.All(x => x < 18).Materialize();
+
+    //All() will fire spontaneously if invalid value encountered in stream, but not synchronously, so the
+    //invalid value might appear before or after the All() sequence outputs.
+    all.Subscribe(x => Console.WriteLine("FOUND ALL: {0}", x));
+
+    //wait until user press RETURN
+    Console.ReadLine();
+
+    //notify the stop message
+    stopperSequence.OnNext(true);
+
+    //wait again to see the result.
+    Console.ReadLine();
+  }
+}
---
+++ ch_5/DebugRetry/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reactive.Linq;
+
+namespace DebugRetry;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    //a finite sequence of 10 values
+    var source = Observable.Interval(TimeSpan.FromSeconds(1))
+        .Take(10)
+        .Select(x => DateTime.Now)
+        .Select(x =>
+          //lets raise some error
+          x.Second % 5 == 0 ? throw new ArgumentException("Wrong milliseconds value") : x)
+        //restart he sourcing sequence on error (max 3 times)
+        .Retry(3)
+        //materialize to read message metadata
+        .Materialize();
+
+    source.Subscribe(x => Console.WriteLine(x));
+    Console.ReadLine();
+  }
+}
