8438cada17057aca670798e633163d602502c1be
Rohan Parkes <rparkes03@gmail.com>
2026-06-06 12:15:13 +1000

    Fixed event handler issue.

---
+++ ch_6/FromEventPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reactive.Linq;
+
+namespace FromEventPattern;
+
+internal class Program
+{
+  public static event EventHandler? UserIsTiredEvent;
+  static void Main(string[] args)
+  {
+    //raise the event in 5 seconds
+    Task.Factory.StartNew(() =>
+    {
+      Thread.Sleep(5000);
+
+      //check event is handled
+      UserIsTiredEvent?.Invoke("Program.Main", new EventArgs());
+    });
+
+    //classic event handler registration
+    UserIsTiredEvent += EventHandler1;
+
+    //reactive registration
+    var eventSequence = Observable.FromEventPattern(typeof(Program), "UserIsTiredEvent");
+
+    //some output
+    eventSequence.Materialize().Subscribe(x => Console.WriteLine("From Rx: {0}", x));
+
+    Console.ReadLine();
+  }
+
+  static void EventHandler1(object? o, EventArgs e)
+  {
+    Console.WriteLine("Handling for object {0}", o);
+  }
+}
---
+++ ch_6/XamlApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace XamlApp1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window, INotifyPropertyChanged
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      DataContext = this;
+
+      //command definition
+      ChangeValueCommand = new RoutedCommand(Guid.NewGuid().ToString(), typeof(MainWindow));
+      //command binding registration
+      CommandBindings.Add(new CommandBinding(ChangeValueCommand, OnChangeValueCommand));
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    //classic WPF implementation
+    private void OnChangeValueCommand(object sender, ExecutedRoutedEventArgs e)
+    {
+      Result += Convert.ToInt32(e.Parameter);
+      //notify value update
+      Notify("Result");
+    }
+
+    public ICommand ChangeValueCommand { get; set; }
+    public int Result { get; set; }
+
+    //value update notification
+    void Notify([CallerMemberName] string name = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+  }
+}
