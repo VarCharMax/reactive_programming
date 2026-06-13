210d656db7fc91d4906be6a0f54a3e50f74dac44
Rohan Parkes <rparkes03@gmail.com>
2026-06-13 12:15:50 +1000

    More projects

---
+++ ch_6/FromEvent/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reactive.Linq;
+
+namespace FromEvent;
+
+internal class Program
+{
+  static void Main(string[] args)
+  {
+    //the action from the FromEvent
+    Action<string>? fromEventAction = null;
+
+    //setup the FromEvent sequence
+    Observable.FromEvent<string>(
+        //register the inner action
+        innerAction => fromEventAction = innerAction,
+        //unregister the inner action
+        innerAction => fromEventAction = null)
+        .Subscribe(x => Console.WriteLine("-> {0}", x));
+
+    int i = 0;
+    while (true)
+    {
+      //invoke the inner action
+      fromEventAction?.Invoke(DateTime.Now.ToString());
+      Thread.Sleep(1000);
+
+      if (++i == 5)
+      {
+        fromEventAction = null;
+        break;
+      }
+    }
+  }
+}
---
+++ ch_6/StaticEvent/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive.Linq;
+
+namespace StaticEvent
+{
+  internal class Program
+  {
+    public static event Action? MyStaticEvent;
+
+    static void Main(string[] args)
+    {
+      //event sequence
+      var sequence = Observable.FromEvent(
+          //register the inner action as handler of the static event
+          x => MyStaticEvent += x,
+          //unregister the inner action from the static event
+          x => MyStaticEvent -= x);
+
+      //observer
+      sequence.Subscribe(unit => Console.WriteLine(unit));
+
+      //manually raise the event
+      MyStaticEvent?.Invoke();
+
+      Console.ReadLine();
+    }
+  }
+}
