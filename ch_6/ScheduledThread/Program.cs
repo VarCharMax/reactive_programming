b4404d7d88b0dd9ce03d77e8e815cd1054006984
Rohan Parkes <rparkes03@gmail.com>
2026-06-14 14:06:23 +1000

    More projects.

---
+++ ch_6/ScheduledThread/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace ScheduledThread
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var scheduler = Scheduler.Default;
+
+      var loopBasedSequence = Observable.Create<DateTime>(x =>
+      {
+        int cnt = 0;
+        while (true)
+        {
+          Console.WriteLine("{0} -> Yielding new value...", Environment.CurrentManagedThreadId);
+          x.OnNext(DateTime.Now);
+          Thread.Sleep(1000);
+
+          if (++cnt == 5)
+          {
+            x.OnCompleted();
+            break;
+          }
+        }
+        return Disposable.Empty;
+      });
+
+      loopBasedSequence.SubscribeOn(NewThreadScheduler.Default).Subscribe(x => Console.WriteLine("{0} -> {1}", Environment.CurrentManagedThreadId, x));
+      loopBasedSequence.SubscribeOn(NewThreadScheduler.Default).Subscribe(x => Console.WriteLine("{0} -> {1}", Environment.CurrentManagedThreadId, x));
+      loopBasedSequence.SubscribeOn(NewThreadScheduler.Default).Subscribe(x => Console.WriteLine("{0} -> {1}", Environment.CurrentManagedThreadId, x));
+
+      Console.ReadLine();
+    }
+  }
+}
